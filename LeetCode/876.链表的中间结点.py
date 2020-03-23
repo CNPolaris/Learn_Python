4208a2112e61d96353bb404eeb0679c204a66270
CNPolaris <42991837+CNPolaris@users.noreply.github.com>
2020-03-23 22:06:10 +0800

    LeetCode 2020/3/22 17:11 快慢指针leetcode打卡
    Signed-off-by: CNPolaris <42991837+CNPolaris@users.noreply.github.com>

--- LeetCode/876.链表的中间结点.py
+++ LeetCode/876.链表的中间结点.py
@@ -9,13 +9,12 @@
 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
 如果有两个中间结点，则返回第二个中间结点。
 '''
-
-
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
+
 
 
 class Solution(object):
