90538dac9bcd3943ff956c19435938de99ea6264
CNPolaris <42991837+CNPolaris@users.noreply.github.com>
2020-03-26 11:28:37 +0800

    LeetCode 2020/3/26 11:28 leetcode练习
    Signed-off-by: CNPolaris <42991837+CNPolaris@users.noreply.github.com>

---
+++ LeetCode/1281.整数的各位积和差.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*-coding: utf-8 -*-
+# @Time    : 2020/3/26 9:58
+# @FileName: 1281.整数的各位积和差.py
+# @Author  : CNTian
+# @Github  : https://github.com/CNPolaris
+# @Email   ：1875091912@qq.com
+'''
+给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+'''
+class Solution(object):
+    def subtractProductAndSum(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        n=str(n)
+        sum,ans=0,1
+        for i in n:
+            sum+=eval(i)
+            ans*=eval(i)
+        return ans-sum
+
+if __name__=='__main__':
+    test=Solution()
+    n = 234
+    print(test.subtractProductAndSum(n))
