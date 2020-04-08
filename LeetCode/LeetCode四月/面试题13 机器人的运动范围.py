bbc5383a605079763f9c6fe2fde184b509563fbd
CNPolaris <42991837+cnpolaris@users.noreply.github.com>
2020-04-08 10:48:54 +0800

    leetcode打卡
    Signed-off-by: CNPolaris <42991837+CNPolaris@users.noreply.github.com>

---
+++ LeetCode/LeetCode四月/面试题13 机器人的运动范围.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/8 10:43
+# @FileName: 面试题13 机器人的运动范围.py
+# @Author  : CNTian
+# @GitHub  ：https://github.com/CNPolaris
+# @Email   : 1875091912@qq.com
+
+'''
+地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。
+一个机器人从坐标 [0, 0] 的格子开始移动，
+它每次可以向左、右、上、下移动一格（不能移动到方格外），
+也不能进入行坐标和列坐标的数位之和大于k的格子。
+例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。
+但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+'''
+import unittest
+def digitsum(n):
+    ans = 0
+    while n:
+        ans += n % 10
+        n //= 10
+    return ans
+
+class Solution:
+    def movingCount(self, m: int, n: int, k: int) -> int:
+        from queue import Queue
+        q = Queue()
+        q.put((0, 0))
+        s = set()
+        while not q.empty():
+            x, y = q.get()
+            if (x, y) not in s and 0 <= x < m and 0 <= y < n and digitsum(x) + digitsum(y) <= k:
+                s.add((x, y))
+                for nx, ny in [(x + 1, y), (x, y + 1)]:
+                    q.put((nx, ny))
+        return len(s)
+
+class Test(unittest.TestCase):
+    def Test_1(self):
+        test=Solution()
+        m,n,k= 2, 3,1
+        self.assertEquals(test.movingCount(m,n,k),3)
+if __name__=='__main__':
+    unittest.main()
